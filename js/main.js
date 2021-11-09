574c29e57e7709a6f3ee897a5b9b6c2ca0428aed
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-09 13:41:14 +0100

    aggiunto timer

--- js/main.js
+++ js/main.js
@@ -6,17 +6,25 @@
 // genero un array di 5 numeri diversi casuali tra 1 e 99
 const daIndovinare = [];
 numsGen(daIndovinare, 5, 1, 99);
-console.log(daIndovinare);
+console.log('numeri da indovinare', daIndovinare);
 
 // stampo i numeri in pagina
 const container = document.getElementById('container');
 container.innerHTML = daIndovinare;
 
 // faccio partire un timer da 30 secondi
+const userNums = [];
+setTimeout((num) => {
 
+    // passati 30 secondi chiedo all'utente per 5 volte un numero da indovinare
+    for (let i = 0; i < 5; i++) {
+        num = parseInt(prompt('Inserisci un nuemero'));
 
-// passati 30 secondi chiedo all'utente per 5 volte un numero da indovinare
-// pusho i numeri inseriti in un array
+        // pusho i numeri inseriti in un array
+        userNums.push(num);
+    }
+    console.log("nuemri inseriti dall'utente", userNums);
+}, 30000);
 
 // confronto i due array
 // se trovo numeri uguali li stampo in pagina
