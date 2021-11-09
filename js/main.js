b9d2504cc57f884448c7f45989a99684e899f9c0
BarbonFisso <barbieri.mat@hotmail.it>
2021-11-09 13:14:36 +0100

    aggiunto analisi script js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,15 @@
 // Da lì parte un timer di 30 secondi.
 // Dopo 30 secondi l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// genero un array di 5 numeri diversi casuali tra 1 e 99
+
+// stampo i numeri in pagina
+
+// faccio partire un timer da 30 secondi
+
+// passati 30 secondi chiedo all'utente per 5 volte un numero da indovinare
+// pusho i numeri inseriti in un array
+
+// confronto i due array
+// se trovo numeri uguali li stampo in pagina
